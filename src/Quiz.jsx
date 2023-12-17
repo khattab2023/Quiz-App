5aaea980b6be5110e211d4d095c09d445cd14ff2
Ahmed <a.khattab2021@gmail.com>
2023-12-17 03:01:02 +0100

    Show result and score

--- src/Quiz.jsx
+++ src/Quiz.jsx
@@ -64,6 +64,20 @@ const onClickNext = () => {
         </div>
       </>) : <div className="result">
         <h3>Result</h3>
+        <p>
+          Total Questions <span>{questions.length}</span>
+        </p>
+        <p>
+          Total Score <span>{result.score}</span>
+        </p>
+        <p>
+          Correct Answers <span>{result.correctAnswers}</span>
+        </p>
+        <p>
+          Wrong Answers <span>{result.wrongAnswers}</span>
+        </p>
+        
+        
         </div>}
     
     </div>;
