b4bde1c704c8258dde947e091eed23f890c7eec4
Ahmed <a.khattab2021@gmail.com>
2023-12-20 14:40:03 +0100

    create answer timer

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
-import Quiz from "./Quiz"
+import Quiz from "./components/Quiz/Quiz";
 import { jsQuizz } from "./constants";
 
 function App() {
   return <Quiz questions={jsQuizz.questions} />;
-  
 }
 
 export default App;
