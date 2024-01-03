48c7b5108e277b86aadd1a0b18cfef00f6aa65bf
khattab <a.khattab2021@gmail.com>
2024-01-03 12:22:23 +0100

    fix colors

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,9 @@
 import Quiz from "./components/Quiz/Quiz";
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 import { jsQuizz } from "./constants";
 
 function App() {
 
-  // const [question, setQuestions] = useState([]);
-
-  // useEffect (() => {
-  //   getQuestions();
-  
-  // }, []);
-  
-  // const getQuestions = async () => {
-  //   try {
-  //     const response = await fetch(
-  //       "https://644982a3e7eb3378ca4ba471.mockapi.io/questions"
-  //       );
-  //     const questionsResponse = await response.json();
-  //     console.log(questionsResponse);
-  //     setQuestions(questionsResponse);
-  //   } catch (error) {
-  //     console.log (error);
-  //   }
-  // };
-  
-  //   return questions.length && <Quiz questions={questions} />;
     return <Quiz questions={jsQuizz.questions} />;
   }
   
